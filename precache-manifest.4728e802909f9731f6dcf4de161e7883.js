9c7dcab580f00a87c109705527d6113d05ab0035
jaymistry0090 <jaymistry409@gmail.com>
2020-09-11 18:35:15 +0530

    Updates

---
+++ precache-manifest.4728e802909f9731f6dcf4de161e7883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e396d2547cdfa4dc33d30ee247cb153d",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "7e9e7280bb8814e086f4",
+    "url": "/project/static/css/main.9bbb898b.chunk.css"
+  },
+  {
+    "revision": "d3153322d81fe97bfa04",
+    "url": "/project/static/js/2.8643b9a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project/static/js/2.8643b9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9e7280bb8814e086f4",
+    "url": "/project/static/js/main.88a3648a.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  }
+]);
